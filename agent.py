f2f1f038e4f3b44a03169b1c792adc2dceb8368a
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-17 20:56:24 -0500

    Continued creating the AI agent
    Continued creating the AI agent

--- agent.py
+++ agent.py
@@ -12,11 +12,21 @@ LEARNINGRATE = 0.001
 class Agent:
 
     def __init__(self):
+        # Related to the epsilon-greedy action selection procedure in the Q-learning algorithm
+        # Introduces randomness into the algorithm
         self.epsilon = 0
+
         # Discount rate
         self.gamma = 0
+
+        # Number of gane iterations
         self.numGames = 0
+
+        # If memory is exceeded, popleft()
         self.memory = deque(maxlen=MAXMEMORY)
+
+    def getAction(self, state):
+        pass
 
     def getState(self, game):
         pass
@@ -24,25 +34,61 @@ class Agent:
     def remember(self, state, action, reward, nextState, gameOver):
         pass
 
-    def trainLongMemory(self):
+    def trainShortTermMemory(self, state, action, reward, nextState, gameOver):
         pass
 
-    def trainShortMemory(self):
-        pass
-
-    def getAction(self, state):
+    def trainLongTermMemory(self):
         pass
 
 
 def train():
-    bestScore = 0
+    # Initialize varibles for the high score and total score
+    highScore = 0
     totalScore = 0
+
+    #
     plotScores = []
     plotMeanScores = []
+
+    #
     agent = Agent()
     game = Game()
+
+    #
     while True:
-        pass
+
+        # Get the old state
+        oldState = agent.getState(game)
+
+        # Get the action
+        action = agent.getAction(oldState)
+
+        # Preform the action and get the new state
+        reward, gameOver, score = game.play(action)
+        newState = agent.getState(game)
+
+        # Train the short term memory
+        agent.trainShortTermMemory(
+            oldState, action, reward, newState, gameOver)
+
+        #
+        agent.remember(oldState, action, reward, newState, gameOver)
+
+        # If the game is over
+        if gameOver:
+
+            # Train the long term memory and plot the results
+            game.reset()
+            agent.numGames += 1
+            agent.trainLongTermMemory()
+
+            # If new high score
+            if score > highScore:
+                highScore = score
+
+            # Print the game iteration, game score, and high score
+            print('Game: ', agent.numGames, ', Score:',
+                  score, ', High Score:', highScore)
 
 
 if __name__ == '__main__':
