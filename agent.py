8fdb174ed90cb7350cefdf43a3c13cf593b1861b
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-17 22:31:01 -0500

    Created the state for the AI agent
    Created the state for the AI agent

--- agent.py
+++ agent.py
@@ -29,7 +29,56 @@ class Agent:
         pass
 
     def getState(self, game):
-        pass
+        # Store the head of the snake
+        head = game.snake[0]
+
+        # Create points in all 4 directions around the head to be used to check for potential dangers
+        pointR = Point(head.x + 20, head.y)
+        pointL = Point(head.x - 20, head.y)
+        pointU = Point(head.x, head.y - 20)
+        pointD = Point(head.x, head.y + 20)
+
+        # Create boolean variables which express the current direction
+        directionR = game.direction == Direction.RIGHT
+        directionL = game.direction == Direction.LEFT
+        directionU = game.direction == Direction.UP
+        directionD = game.direction == Direction.DOWN
+
+        # Create the state list
+        state = [
+            # Danger is straight
+            (directionR and game.is_collision(pointR)) or
+            (directionL and game.is_collision(pointL)) or
+            (directionU and game.is_collision(pointU)) or
+            (directionD and game.is_collision(pointD)),
+
+            # Danger is right
+            (directionU and game.is_collision(pointR)) or
+            (directionD and game.is_collision(pointL)) or
+            (directionL and game.is_collision(pointU)) or
+            (directionR and game.is_collision(pointD)),
+
+            # Danger is left
+            (directionD and game.is_collision(pointR)) or
+            (directionU and game.is_collision(pointL)) or
+            (directionR and game.is_collision(pointU)) or
+            (directionL and game.is_collision(pointD)),
+
+            # Move direction
+            directionL,
+            directionR,
+            directionU,
+            directionD,
+
+            # Food location
+            game.food.x < game.head.x,
+            game.food.x > game.head.x,
+            game.food.y < game.head.y,
+            game.food.y > game.head.y
+        ]
+
+        # Return list as a numpy array
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, nextState, gameOver):
         pass
