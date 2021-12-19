32e3c16d40965fe78b50dbda903f6a998a03c452
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-19 16:14:34 -0500

    Finished creating a LinearQNet model and trainer
    Finished creating a LinearQNet model and trainer

--- model.py
+++ model.py
@@ -37,4 +37,32 @@ class QTrainer:
             model.parameters(), learningRate=self.learningRate)
 
     def trainStep(self, state, action, reward, nextState, gameOver):
-        pass
+        stateTensor = torch.tensor(state, dtype=torch.float)
+        actionTensor = torch.tensor(action, dtype=torch.float)
+        rewardTensor = torch.tensor(reward, dtype=torch.long)
+        nextStateTensor = torch.tensor(nextState, dtype=torch.float)
+
+        if len(stateTensor.shape) == 1:
+            stateTensor = torch.unsqueeze(stateTensor, 0)
+            actionTensor = torch.unsqueeze(actionTensor, 0)
+            rewardTensor = torch.unsqueeze(rewardTensor, 0)
+            nextStateTensor = torch.unsqueeze(nextStateTensor, 0)
+            gameOver = (gameOver, )
+
+        # Get predicted Q value with the current state
+        prediction = self.model(stateTensor)
+
+        # QNew = reward + gamma * max(next predicted Q value)
+        predictionClone = prediction.clone()
+        for i in range(len(gameOver)):
+            QNew = rewardTensor[i]
+            if not gameOver[i]:
+                qNew = rewardTensor[i] + self.gamma * \
+                    torch.max(self.model(nextStateTensor[i]))
+            predictionClone[i][torch.argmax(actionTensor[i]).item()] = QNew
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(predictionClone, prediction)
+        loss.backward()
+
+        self.optimizer.step()
